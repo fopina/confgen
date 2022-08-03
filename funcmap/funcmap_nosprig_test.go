01afddfb04b2d060ccb540248982bdf153f82b9c
Filipe Pina <fopina@gmail.com>
2022-08-03 01:37:04 +0100

    add `envFile` function to support `_FILE` suffix on environment variables (for easier docker secrets, for instance)

--- funcmap/funcmap_nosprig_test.go
+++ funcmap/funcmap_nosprig_test.go
@@ -5,6 +5,7 @@ package funcmap
 
 import (
 	"bytes"
+	"io/ioutil"
 	"os"
 	"testing"
 	"text/template"
@@ -12,21 +13,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func newTemplate() *template.Template {
+	return template.New("test").Funcs(CommonFuncMap()).Funcs(FuncMap())
+}
+
 func TestEnvNotSet(t *testing.T) {
-	template, err := template.New("test").Funcs(FuncMap()).Parse(`Hello {{ env "UNKNOWN" }}`)
+	template, err := newTemplate().Parse(`Hello {{ env "UNKNOWN" }}`)
 	assert.Nil(t, err)
 	var buf bytes.Buffer
 	err = template.Execute(&buf, nil)
 	assert.Nil(t, err)
-	assert.Equal(t, buf.String(), "Hello ")
+	assert.Equal(t, "Hello ", buf.String())
 }
 
 func TestEnv(t *testing.T) {
 	os.Setenv("UNKNOWN", "World")
-	template, err := template.New("test").Funcs(FuncMap()).Parse(`Hello {{ env "UNKNOWN" }}`)
+	template, err := newTemplate().Parse(`Hello {{ env "UNKNOWN" }}`)
 	assert.Nil(t, err)
 	var buf bytes.Buffer
 	err = template.Execute(&buf, nil)
 	assert.Nil(t, err)
-	assert.Equal(t, buf.String(), "Hello World")
+	assert.Equal(t, "Hello World", buf.String())
+}
+
+func TestEnvFile(t *testing.T) {
+	os.Setenv("UNKNOWN", "World")
+	os.Setenv("UNKNOWN_FILE", "World")
+	template, err := newTemplate().Parse(`Hello {{ envFile "UNKNOWN" }}`)
+	assert.Nil(t, err)
+	var buf bytes.Buffer
+	err = template.Execute(&buf, nil)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello World", buf.String())
+
+	os.Setenv("UNKNOWN", "")
+	buf.Reset()
+	err = template.Execute(&buf, nil)
+	assert.NotNil(t, err)
+
+	file, err := ioutil.TempFile("", "prefix")
+	assert.Nil(t, err)
+	defer os.Remove(file.Name())
+
+	os.WriteFile(file.Name(), []byte("There"), 0644)
+	os.Setenv("UNKNOWN_FILE", file.Name())
+	buf.Reset()
+
+	err = template.Execute(&buf, nil)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello There", buf.String())
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path"
 	"text/template"
+
 	"github.com/fopina/confgen/funcmap"
 )
 
